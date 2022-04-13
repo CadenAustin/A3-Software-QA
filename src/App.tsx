1457f01b3674fe2a45a392fa4264059f8912666c
Caden Austin <cadenbryant@hotmail.com>
2022-04-12 22:09:27 -0500

    Linting Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import BMI from "./components/BMI" 
+import BMI from './components/BMI';
 
 function App() {
   return (
--- src/helpers/bmi.tsx
+++ src/helpers/bmi.tsx
@@ -1,36 +1,35 @@
 export interface BmiInput {
-    h_feet: number;
-    h_inch: number;
-    weight: number;
+  hFeet: number;
+  hInch: number;
+  weight: number;
 }
 
 export interface BmiOutput {
-    bmi: number;
-    category: string;
+  bmi: number;
+  category: string;
 }
 
-function bmi_classify(bmi: number): string {
-    let output: string;
-    if (bmi < 18.5) output = "Underweight";
-    else if (bmi <= 24.9) output = "Normal";
-    else if (bmi <= 29.9) output = "Overweight";
-    else if (bmi >= 30) output = "Obese";
-    else return "Invalid";
-    return output
+function BmiClassify(bmi: number): string {
+  let output: string;
+  if (bmi < 18.5) output = 'Underweight';
+  else if (bmi <= 24.9) output = 'Normal';
+  else if (bmi <= 29.9) output = 'Overweight';
+  else if (bmi >= 30) output = 'Obese';
+  else return 'Invalid';
+  return output;
 }
 
-function bmi_calculator(input: BmiInput): BmiOutput {
-    const {h_feet, h_inch, weight} = input
-    let kilos = weight * 0.45;
-    let inches = (h_feet * 12) + h_inch
-    let meters_squared = (inches * 0.025) ** 2;
+function BmiCalculator(input: BmiInput): BmiOutput {
+  const { hFeet, hInch, weight } = input;
+  const kilos = weight * 0.45;
+  const inches = (hFeet * 12) + hInch;
+  const metersSquared = (inches * 0.025) ** 2;
 
-    let bmi = Math.round((kilos / meters_squared) * 10) / 10
-    console.log(bmi, kilos/meters_squared)
-    return {
-        bmi: bmi,
-        category: bmi_classify(bmi),
-    }
+  const bmi = Math.round((kilos / metersSquared) * 10) / 10;
+  return {
+    bmi,
+    category: BmiClassify(bmi),
+  };
 }
 
-export {bmi_classify, bmi_calculator}
+export { BmiClassify, BmiCalculator };
